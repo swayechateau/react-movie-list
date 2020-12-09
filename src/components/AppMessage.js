b68cd282fd252c2f501863e143e486bc283e23b1
Swaye Chateau <swaye@kitechltd.com>
2020-12-09 03:22:28 +0000

    added app message css

--- src/components/AppMessage.js
+++ src/components/AppMessage.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
+import './AppMessage.css';
 class AppMessage extends Component {
 
   render() {
     return (
-      <div >
+      <div className="app-message">
         Please Search for a movie in the above search bar!
     </div>
     );
